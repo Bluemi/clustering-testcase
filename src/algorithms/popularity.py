3dc8994346ed4ddfb8a4c544da189a0c8dd6a87f
Bluemi <bruno.schilling@protonmail.ch>
2023-06-11 14:40:05 +0200

    removed prints

--- src/algorithms/popularity.py
+++ src/algorithms/popularity.py
@@ -14,17 +14,10 @@ class Popularity(Algorithm):
         quantized_points = (points / self.quantization).astype(int) * self.quantization + self.quantization // 2
         unique_values, counts = np.unique(quantized_points, axis=0, return_counts=True)
 
-        # print(f'unique_values.shape: {unique_values.shape}')
-        # print(f'num_chunks: {self.num_chunks}')
-
-        # print(f'counts.shape: {counts.shape}')
-        # print(f'unique_values.shape: {unique_values.shape}')
         most_common_indices = np.argsort(counts)[::-1]
         used_most_common_indices = most_common_indices[:self.num_chunks]
-        # print(f'used_most_common_indices.shape: {used_most_common_indices.shape}')
 
         centers = unique_values[used_most_common_indices]
-        start_time = time.time()
         clustered_points = cluster_points_by_centers(points, centers)
-        end_time = time.time()
+
         return centers, clustered_points
