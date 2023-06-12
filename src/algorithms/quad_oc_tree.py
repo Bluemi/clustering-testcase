365d4cfd2c8adae74b0a3a62117cacb9bc9a87b4
Bluemi <bruno.schilling@protonmail.ch>
2023-06-12 11:25:32 +0200

    fixed octree

--- src/algorithms/quad_oc_tree.py
+++ src/algorithms/quad_oc_tree.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from algorithms.algorithm import Algorithm, cluster_points_by_centers
+from algorithms.algorithm import Algorithm
 
 
 class QuadOcTree(Algorithm):
@@ -9,34 +9,37 @@ class QuadOcTree(Algorithm):
 
     def cluster(self, points: np.ndarray):
         num_dims = points.shape[-1]
-        chunks = [(np.zeros(num_dims), np.zeros(num_dims) + 256, points)]
+        point_indices = np.arange(len(points))
+        chunks = [(np.zeros(num_dims), np.zeros(num_dims) + 256, point_indices)]
         while len(chunks) + (2**num_dims) - 1 <= self.num_chunks:
             full_chunk_index, full_chunk = max(enumerate(chunks), key=lambda c: c[1][2].shape[0])
             del chunks[full_chunk_index]  # remove chunk from list
             chunks.extend(
-                _split_chunk(num_dims, *full_chunk)
+                _split_chunk(num_dims, *full_chunk, points)
             )
 
         centers = []
+        clustered_points = np.empty_like(points)
         for chunk in chunks:
             center = (chunk[0] + chunk[1]) / 2
             centers.append(center)
+            clustered_points[chunk[2]] = center
 
         centers = np.array(centers)
-
-        return centers, cluster_points_by_centers(points, centers)
+        return centers, clustered_points
 
     def name(self) -> str:
         return 'OcTree'
 
 
-def _split_chunk(num_dims: int, parent_chunk_start, parent_chunk_end, points):
+def _split_chunk(num_dims: int, parent_chunk_start, parent_chunk_end, point_indices, points):
     """
 
     :param num_dims: The number of dimensions D for each point.
     :param parent_chunk_start: np.ndarray with shape [D,], marking the beginning of the parent chunk
     :param parent_chunk_end: np.ndarray with shape [D,], marking the end of the parent chunk
-    :param points: The points contained in the parent chunk with shape [N, D]
+    :param point_indices: The indices into points of the given parent chunk with shape [N, D]
+    :param points: All points. To get points of parent chunk index with point_indices
     :return: A list with length 2**D. Each element stands for a child chunk: (chunk_start, chunk_end, points).
     """
     quantization_per_dim = 2
@@ -52,8 +55,8 @@ def _split_chunk(num_dims: int, parent_chunk_start, parent_chunk_end, points):
     meshgrid_ends = np.stack(np.meshgrid(*child_chunk_ends.T, indexing='ij'), axis=num_dims).reshape(-1, num_dims)
 
     # bigger_than_start_indices has shape [N, NC, D]
-    bigger_than_start_pos = np.all(meshgrid_starts[None, :] <= points[:, None], axis=2)
-    smaller_than_end_pos = np.all(meshgrid_ends[None, :] > points[:, None], axis=2)
+    bigger_than_start_pos = np.all(meshgrid_starts[None, :] <= points[point_indices, None], axis=2)
+    smaller_than_end_pos = np.all(meshgrid_ends[None, :] > points[point_indices, None], axis=2)
 
     # point contained has shape [N, NC]
     point_contained = np.logical_and(bigger_than_start_pos, smaller_than_end_pos)
@@ -61,8 +64,8 @@ def _split_chunk(num_dims: int, parent_chunk_start, parent_chunk_end, points):
     chunks = []
     chunk_index = 0
     for start_pos, end_pos in zip(meshgrid_starts, meshgrid_ends):
-        chunk_points = points[point_contained[:, chunk_index]]
-        chunk = (start_pos, end_pos, chunk_points)
+        chunk_point_indices = point_indices[point_contained[:, chunk_index]]
+        chunk = (start_pos, end_pos, chunk_point_indices)
         chunks.append(chunk)
         chunk_index += 1
 
