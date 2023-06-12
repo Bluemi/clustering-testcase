d981ed7adfdf0dc261372a91d25b54e08f8def64
Bluemi <bruno.schilling@protonmail.ch>
2023-06-12 12:17:39 +0200

    add lbg algorithm

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,8 +1,9 @@
 from algorithms.generalized_lloyd import GeneralizedLloyd
 from algorithms.kmeans import KMeans
+from algorithms.lbg_algorithm import LBGAlg
 from algorithms.linear_quantisation import LinearQuantization
 from algorithms.median_cut import MedianCut
 from algorithms.popularity import Popularity
 from algorithms.quad_oc_tree import QuadOcTree
 
-ALGORITHMS = [LinearQuantization, Popularity, QuadOcTree, MedianCut, KMeans]
+ALGORITHMS = [LinearQuantization, Popularity, QuadOcTree, MedianCut, KMeans, LBGAlg]
---
+++ src/algorithms/lbg_algorithm.py
@@ -0,0 +1,27 @@
+"""
+The implementation of this algorithm is used from https://github.com/internaut/py-lbg/blob/master/lbg.py
+"""
+import numpy as np
+import scipy.cluster.vq
+import algorithms.lbg
+
+from algorithms.algorithm import Algorithm, cluster_points_by_centers
+
+
+class LBGAlg(Algorithm):
+    def __init__(self, num_chunks):
+        super().__init__(num_chunks)
+
+    def cluster(self, points: np.ndarray):
+        whitened_points = scipy.cluster.vq.whiten(points)
+        using_num_chunks = int(2**np.floor(np.log2(self.num_chunks)))
+        centers_whitened, _, _ = algorithms.lbg.generate_codebook(
+            whitened_points, using_num_chunks, epsilon=0.01
+        )
+
+        centers = centers_whitened * np.std(points, axis=0)
+
+        return centers, cluster_points_by_centers(points, centers)
+
+    def name(self) -> str:
+        return 'LBGAlg'
